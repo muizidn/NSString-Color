6c83a3b68e816bed524c25f39aeb8790600cdae7
muizidn <muiz.idn@gmail.com>
2021-03-09 08:39:08 +0700

    add xcodeproj

---
+++ NSString-Color/NSString-Color/NSString_Color.h
@@ -0,0 +1,19 @@
+//
+//  NSString_Color.h
+//  NSString-Color
+//
+//  Created by Muis on 09/03/21.
+//  Copyright © 2021 Muis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NSString_Color.
+FOUNDATION_EXPORT double NSString_ColorVersionNumber;
+
+//! Project version string for NSString_Color.
+FOUNDATION_EXPORT const unsigned char NSString_ColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSString_Color/PublicHeader.h>
+
+#import "NSString+Color.h"
